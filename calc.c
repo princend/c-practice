a3a3ba3f8cf980968a71ed114bf122f585ffd331
Princend <waxdji3g4y3c04@gmail.com>
2024-09-18 10:58:49 +0800

    指標練習1

--- calc.c
+++ calc.c
@@ -1,53 +1,131 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-float divide (){
-    float value1=10;
-    int value2=4;
-    float result =0;
-    result=value1/value2;
-    printf("%f\n",result);
-    return 0;
+// float divide (){
+//     float value1=10;
+//     int value2=4;
+//     float result =0;
+//     result=value1/value2;
+//     printf("%f\n",result);
+//     return 0;
+// }
+
+float calculateCar()
+{
+    int people = 11;
+    int capacity = 5;
+    int cars = 0;
+    int leftPeople = 0;
+    cars = people / capacity;
+    leftPeople = people % capacity;
+    printf("ï¿½Ý­n%dï¿½xï¿½ï¿½\n", cars);
+    printf("ï¿½ï¿½%dï¿½Hï¿½Sï¿½fï¿½ì¨®\n", leftPeople);
 }
 
-float calculateCar(){
-    int people =11;
-    int capacity=5;
-    int cars=0;
-    int leftPeople=0;
-    cars = people/capacity;
-    leftPeople =people % capacity;
-    printf("»Ý­n%d¥x¨®\n",cars);
-    printf("³Ñ%d¤H¨S·f¨ì¨®\n",leftPeople);
-}
-
-float discount(){
-    int total=0;
-    int product1_price=12;
-    int product2_price=200;
-    total+=product1_price;
-    total+=product2_price;
+float discount()
+{
+    int total = 0;
+    int product1_price = 12;
+    int product2_price = 200;
+    total += product1_price;
+    total += product2_price;
     float discount = 0.5;
 }
 
-void coercion(){
-    printf("%d\n",(int) 2.1);
+void coercion()
+{
+    printf("%d\n", (int)2.1);
 }
 
-void conditionPrintf(){
-    if(true){
+void conditionPrintf()
+{
+    if (true)
+    {
         printf("true");
     }
 }
 
+int add(int a, int b)
+{
+    printf("%d\n", a + b);
+    return 0;
+}
+int minus(int a, int b)
+{
+    printf("%d\n", a - b);
+    return 0;
+}
+int product(int a, int b)
+{
+    printf("%d\n", a * b);
+    return 0;
+}
+int divide(int a, int b)
+{
+    printf("%d\n", a / b);
+    return 0;
+}
 
-void main(){
-    conditionPrintf();
-    // coercion();
-    // calculateCar();
-    // divide();
-    // printf("Discount %f \n",discount);
+float sqr(float a)
+{
+    return a * a;
+}
+
+float div(float a, float b)
+{
+    return a / b;
+}
+
+void inlineFn()
+{
+    inline int hello(void) { printf("hello world\n"); }
+    hello();
+}
+
+int factorialForLoop(int count){
+    int sum=1;
+    if(count<=1)return sum;
+    for (int i = count; i > 0; i--)
+    {
+        sum =sum*i;
+    }
+    printf("%d\n",sum);
+    return sum;
+    
+}
+
+int my_sum(int total){
+    if(total<=0){
+        return total;
+    }
+    else {
+        return total+my_sum(total-1);
+    }
 }
 
 
 
+void main()
+{
+    int aaa=my_sum(10);
+    printf("%d\n",aaa);
+    // factorialForLoop(10);
+    // conditionPrintf();
+    // add(199,315);
+    // minus(623,253);
+    // product(365,124);
+    // divide(85,17);
+    // divide(65,30);
+
+    //     float height=1.68;
+    //     float weight = 60;
+    //    float bmi=div(weight,sqr(1.68)) ;
+    //    printf("%f",bmi);
+    //    inlineFn();
+    // coercion();
+    // calculateCar();
+    // divide();
+    // printf("Discount %f\n" ,discount);
+    // int rect (int a,int b){printf("%d",a*b);};
+    // rect(10,4);
+}
--- pointer.c
+++ pointer.c
@@ -1,17 +1,115 @@
 # include <stdio.h>
 
-void main(){
-    char ssss='s';
-    char aaaa='a';
-    char ssaa[]="sa";
-    char textStr[]="this is a test";
+void my_pointer(){
+    int a =5;
+    float b= 1.2;
+    int *p=&a;
+    float *q=&b;
+
+    printf("a=%d(address:%p)\n",a,&a);
+    printf("b=%f(address:%p)\n",b,&b);
+    printf("p=%p\n",p);
+    printf("q=%p\n",q);
+}
+
+void change(){
+    int a = 12;
+    int *p=&a;
+    printf("%d\n",a);
+    *p=16;
+    printf("%d\n",a);
+}
+
+void test1(){
+   int a= 5;
+   float b= 1.1;
+   int *p=&a;
+   float *q=&b;
+   printf("%d\n",p);
+   return ;
+}
+
+void lab5_2(){
+    int a=5;
+    int b=1;
+    int *p=&a;
+    printf("a=%d;b=%d;*p=%d\n",a,b,*p);
+
+    int *q=&a;
+    *q=12;
+    printf("a=%d; b=%d\n",a,b); //12,1
+    printf("p=%d; *q=%d\n",*p ,*q); //12 ,12
+
+
+}
+
+
+void operartors_in_pointers(){
+    printf("Ex 5-5:Operator +/- Pointers\n");
+    int a= 5;
+    int *p =&a;
+
+    printf("the values of a and its address are %d and %p ,repectively.\n",a,&a);
+    printf("the values of p,dereference and its address are %p ,%d and %p,repectively.\n",p,*p,&p);
+    printf("\nPlus 1 :: \n");
+
+    a=a+1;
+    p=p+1;
+    printf("the values of a and its address are %d and %p ,repectively. \n",a,&a);
+    printf("the values of p,p dereference and its address are %p,%d and %p ,repectively. \n",p,*p,&p);
+}
+
+void adavnced_practice(int value1){
+    int a = value1;
+    int *p =&a;
+    printf("a address is %p\n",&a);
+    printf("a value is %d\n",a);
     
-    char *address = textStr;
-    printf("%c \n",*((char *)(address+3)));
-    *((char *)(address+3))='q';
-    printf("%s",textStr);
-    FILE *file;
-    file = fopen("text.txt","w+");
-    fwrite(&ssss,1,sizeof(ssss),file);
-    fclose(file);
+    unsigned char* q =(char* )p;
+
+    printf("the value of pointer q is %p\n",q);
+    printf("the pointing value  of pointer q is %d\n",*q);
+    printf("the address  of pointer q is %p\n\n",&q);
+
+
+    int range4search =20;
+    int newlineNum =10;
+    printf("==== Memory overview ====");
+    for (int i =-range4search;i<range4search;i++){
+        if(i%newlineNum==0){
+            printf("\n%p\t %d\t",q+i,*(q+i));
+        }
+        else{
+            printf("%d\t",*(q+i));
+        }
+    }
+}
+
+
+void main(){
+    // adavnced_practice(25);
+    // adavnced_practice(255);
+    // adavnced_practice(256);
+    // adavnced_practice(512);
+    // adavnced_practice(1024);
+    // adavnced_practice(1028);
+    // operartors_in_pointers();
+    // lab5_2();
+    // test1();
+    // change();
+    // my_pointer();
+
+    // char ssss='s';
+    // char aaaa='a';
+    // char ssaa[]="sa";
+    // char textStr[]="this is a test";
+    
+    // char *address = textStr;
+    // printf("%c \n",*((char *)(address+3)));
+    // *((char *)(address+3))='q';
+    // printf("%s",textStr);
+    // FILE *file;
+    // file = fopen("text.txt","w+");
+    // fwrite(&ssss,1,sizeof(ssss),file);
+    // fclose(file);
 }
