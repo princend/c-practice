4766ce6c8ef1ce2e6cf0859c9971fcfd87f93637
Princend <waxdji3g4y3c04@gmail.com>
2024-10-21 15:39:48 +0800

    delete node

--- linklist.c
+++ linklist.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct Flight
 {
@@ -10,29 +11,29 @@ typedef struct Flight
     double duration;
 } Flight;
 
-typedef struct Node
+typedef struct FlightNode
 {
     Flight data;
-    struct Node *next;
-} Node;
+    struct FlightNode *next;
+} FlightNode;
 
 void ex14_2()
 {
-    Node a;
+    FlightNode a;
     a.data.frequency = 100;
     printf("%d\n", a.data.frequency);
     printf("%d\n", a.next);
 }
 
-typedef struct Node_1
+typedef struct Node
 {
     char alpha;
-    struct Node_1 *next;
-} Node_1;
+    struct Node *next;
+} Node;
 
 void ex14_3()
 {
-    Node_1 a, c;
+    Node a, c;
     a.alpha = 'a';
     a.next = &c;
 
@@ -42,7 +43,7 @@ void ex14_3()
 
 void ex14_4()
 {
-    Node_1 a, c, t;
+    Node a, c, t;
     a.alpha = 'A';
     a.next = &c;
     c.alpha = 'C';
@@ -50,7 +51,7 @@ void ex14_4()
     t.alpha = 'T';
     t.next = 0;
 
-    Node_1 *now = &a;
+    Node *now = &a;
     while (now)
     {
         printf("%c\n", now->alpha);
@@ -59,7 +60,7 @@ void ex14_4()
     putchar('\n');
 }
 
-void printNode(const Node_1 *head)
+void printNode(const Node *head)
 {
     while (head)
     {
@@ -71,7 +72,7 @@ void printNode(const Node_1 *head)
 
 void ex14_5()
 {
-    Node_1 a, c, t;
+    Node a, c, t;
     a.alpha = 'A';
     a.next = &c;
     c.alpha = 'C';
@@ -85,8 +86,8 @@ void ex14_5()
 void ex14_6()
 {
     char letter[4] = {'A', 'C', 'T'};
-    Node_1 act[3];
-    Node_1 *now = &act[0];
+    Node act[3];
+    Node *now = &act[0];
     for (int i = 0; i < 3; i++)
     {
         now->alpha = letter[i];
@@ -99,19 +100,91 @@ void ex14_6()
     printf("%p %p %p\n", act[0].next, act[1].next, act[2].next);
 }
 
-void builtLLByLoop(char letter[])
+Node *builtLLByLoop(char letter[])
 {
     int size = strlen(letter) + 1;
-    Node_1 linklist[size];
-    Node_1 *now = &linklist[0];
+    Node *linklist = (Node *)malloc(sizeof(Node) * size);
+    if (linklist == NULL)
+    {
+        printf("failed to allocate memory\n");
+        return NULL;
+    }
+    Node *now = &linklist[0];
 
     for (int i = 0; i < size - 1; i++)
     {
         now->alpha = letter[i];
-        now->next = (i == size - 1) ? 0 : &linklist[i + 1];
+        now->next = (i == size - 2) ? NULL : &linklist[i + 1];
         printf("[%d]%c,%p\n", i, now->alpha, now->next);
         now = now->next;
     }
+    return linklist;
+}
+
+void ex14_7()
+{
+    char letter[4] = {'A', 'G', 'O'};
+    char target = 'O';
+    Node *linklist = builtLLByLoop(letter);
+    Node *now = &linklist[0];
+    while (now)
+    {
+        if (now->alpha == target)
+        {
+            printf("found\n");
+            printf("the index is %d\n", now - &linklist[0]);
+            break;
+        }
+
+        now = now->next;
+    }
+}
+
+void ex14_8()
+{
+    char letter[4] = {'A', 'G', 'O'};
+    char target = 'G';
+    Node *linklist = builtLLByLoop(letter);
+    Node *now = &linklist[0];
+    Node x;
+    while (now)
+    {
+        if (now->alpha == target)
+        {
+            printf("found\n");
+            printf("the index is %d\n", now - &linklist[0]);
+            Node *loc = now->next;
+            x.alpha = 'X';
+            x.next = loc;
+            now->next = &x;
+            break;
+        }
+        now = now->next;
+    }
+    printNode(&linklist[0]);
+}
+
+void ex14_9()
+{
+    char letter[5] = {'W', 'E', 'A', 'R'};
+    char target = 'E';
+    Node *linklist = builtLLByLoop(letter);
+    Node *now = &linklist[0];
+    Node *pre = &linklist[0];
+    printNode(&linklist[0]);
+
+    while (now)
+    {
+        if (now->alpha == target)
+        {
+            pre->next = now->next;
+            break;
+        }
+        pre = now;
+        now = now->next;
+    }
+    printNode(&linklist[0]);
+    free(linklist);
 }
 
 void main()
@@ -121,6 +194,7 @@ void main()
     // ex14_4();
     // ex14_5();
     // ex14_6();
-    char letter[7] = {'A', 'C', 'T', 'I', 'O', 'N'};
-    builtLLByLoop(letter);
+    // ex14_7();
+    // ex14_8();
+    ex14_9();
 }
